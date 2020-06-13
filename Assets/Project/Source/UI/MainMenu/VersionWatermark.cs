fab62855078875e416ec3433bf3fbe8a70e54aec
JaeggerMeisster <47319792+JaeggerMeisster@users.noreply.github.com>
2020-06-13 21:21:13 +0200

    added build version watermark

---
+++ Assets/Project/Source/UI/MainMenu/VersionWatermark.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Exa.UI
+{
+    public class VersionWatermark : MonoBehaviour
+    {
+        [SerializeField] private Text versionText;
+
+        private void Awake()
+        {
+            versionText.text = $"Build: {Application.version}";
+        }
+    }
+}
+
