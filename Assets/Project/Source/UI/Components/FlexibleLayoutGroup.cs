c9b9de5980c4b59a782b7a8e57c7f7453a8b1dfd
JaeggerMeisster <47319792+JaeggerMeisster@users.noreply.github.com>
2020-07-05 23:23:47 +0200

    finished flexible grid layout group, probably not gonna use it anyway ¯\_(ツ)_/¯

--- Assets/Project/Source/UI/Components/FlexibleLayoutGroup.cs
+++ Assets/Project/Source/UI/Components/FlexibleLayoutGroup.cs
@@ -6,26 +6,65 @@ namespace Exa.UI.Components
 {
     public class FlexibleLayoutGroup : LayoutGroup
     {
+        public enum FitType
+        {
+            Uniform,
+            Width,
+            Height,
+            FixedRows,
+            FixedColumns
+        }
+
+        public FitType fitType;
         public int rows;
         public int columns;
         public Vector2 cellSize;
+        public Vector2 spacing;
+
+        public bool fitX;
+        public bool fitY;
 
         public override void CalculateLayoutInputHorizontal()
         {
             base.CalculateLayoutInputHorizontal();
 
-            var sqrRt = Mathf.Sqrt(transform.childCount);
-            rows = Mathf.CeilToInt(sqrRt);
-            columns = Mathf.CeilToInt(sqrRt);
+            if (fitType == FitType.Width 
+                || fitType == FitType.Height 
+                || fitType == FitType.Uniform)
+            {
+                var sqrRt = Mathf.Sqrt(transform.childCount);
+                rows = Mathf.CeilToInt(sqrRt);
+                columns = Mathf.CeilToInt(sqrRt);
+            }
+
+            if (fitType == FitType.Width 
+                || fitType == FitType.FixedColumns)
+            {
+                rows = Mathf.CeilToInt(transform.childCount / (float)columns);
+            }
+            if (fitType == FitType.Height 
+                || fitType == FitType.FixedRows)
+            {
+                columns = Mathf.CeilToInt(transform.childCount / (float)rows);
+            }
 
             var parentWidth = rectTransform.rect.width;
             var parentHeight = rectTransform.rect.height;
 
-            var cellWidth = parentWidth / (float)columns;
-            var cellHeight = parentHeight / (float)rows;
+            var cellWidth = 
+                parentWidth / (float)columns 
+                - ((spacing.x / (float)columns) * 2f) 
+                - (padding.left / (float)columns) 
+                - (padding.right / (float)columns);
 
-            cellSize.x = cellWidth;
-            cellSize.y = cellHeight;
+            var cellHeight = 
+                parentHeight / (float)rows 
+                - ((spacing.y / (float)rows) * 2f)
+                - (padding.top / (float)rows)
+                - (padding.bottom / (float)rows);
+
+            cellSize.x = fitX ? cellWidth : cellSize.x;
+            cellSize.y = fitY ? cellHeight : cellSize.y;
 
             var columnCount = 0;
             var rowCount = 0;
@@ -37,8 +76,8 @@ namespace Exa.UI.Components
 
                 var item = rectChildren[i];
 
-                var xPos = (cellSize.x * columnCount);
-                var yPos = (cellSize.y * rowCount);
+                var xPos = (cellSize.x * columnCount) + (spacing.x * columnCount) + padding.left;
+                var yPos = (cellSize.y * rowCount) + (spacing.y * rowCount) + padding.top;
 
                 SetChildAlongAxis(item, 0, xPos, cellSize.x);
                 SetChildAlongAxis(item, 1, yPos, cellSize.y);
