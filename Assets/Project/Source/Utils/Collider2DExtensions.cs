78496670bcd7726b16c7536c2e4b1b3bb116568e
Manuel Jager <manuel.jager@hotmail.nl>
2020-11-21 21:05:47 +0100

    Fixed block density calculation

--- Assets/Project/Source/Utils/Collider2DExtensions.cs
+++ Assets/Project/Source/Utils/Collider2DExtensions.cs
@@ -7,7 +7,7 @@ namespace Exa.Utils
         public static void SetMass(this BoxCollider2D collider, float mass) {
             var size = collider.size;
             var area = size.x * size.y;
-            collider.density = area * mass;
+            collider.density = mass / area;
         }
     }
 }
