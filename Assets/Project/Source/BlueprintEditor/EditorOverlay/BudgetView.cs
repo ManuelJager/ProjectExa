093995d818536a0284f72502c3a3e525ddf65ecb
Manuel Jager <manuel.jager@hotmail.nl>
2021-04-24 14:53:58 +0200

    Added editor budget field animation

--- Assets/Project/Source/BlueprintEditor/EditorOverlay/BudgetView.cs
+++ Assets/Project/Source/BlueprintEditor/EditorOverlay/BudgetView.cs
@@ -1,5 +1,9 @@
-﻿using Exa.Grids.Blocks;
+﻿using DG.Tweening;
+using Exa.Grids.Blocks;
+using Exa.Utils;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Exa.ShipEditor
 {
@@ -7,8 +11,65 @@ namespace Exa.ShipEditor
     {
         public BlockCostsView view;
 
+        private AnimatableField creditsField;
+        private AnimatableField metalsField;
+        
+        private void Awake() {
+            creditsField = new AnimatableField(view.credits, view.creditsBackground);
+            metalsField = new AnimatableField(view.metals, view.metalsBackground);
+        }
+        
         public void SetBudget(BlockCosts budget) {
             view.Refresh(budget);
+            
+            creditsField.SetActiveColor(budget.creditCost >= 0);
+            metalsField.SetActiveColor(budget.metalsCost >= 0);
+        }
+    }
+    
+    public class AnimatableField
+    {
+        private Graphic text;
+        private Graphic image;
+        
+        private Color initialTextColor;
+        private Color initialImageColor;
+
+        private Tween textTween;
+        private Tween imageTween;
+        
+        private bool active;
+
+        public AnimatableField(Graphic text, Graphic image) {
+            this.text = text;
+            this.image = image;
+            
+            initialTextColor = text.color;
+            initialImageColor = image.color;
+            
+            active = true;
+        }
+
+        public void SetActiveColor(bool active) {
+            if (active != this.active) {
+                if (active) {
+                    Reset();
+                }
+                else {
+                    Animate(Colors.RoseVale, Colors.RoseVale.SetAlpha(100f / 256f), 0.5f);
+                }
+            }
+
+            this.active = active;
+        }
+
+        private void Reset() {
+            Animate(initialTextColor, initialImageColor, 0.2f);
+        }
+
+        private void Animate(Color textColor, Color imageColor, float duration) {
+            text.DOColor(textColor, duration).Replace(ref textTween);
+            image.DOColor(imageColor, duration).Replace(ref imageTween);
         }
     }
 }
--- Assets/Project/Source/UI/Tooltips/ComponentViews/BlockCostsView.cs
+++ Assets/Project/Source/UI/Tooltips/ComponentViews/BlockCostsView.cs
@@ -2,12 +2,16 @@ using Exa.Grids.Blocks;
 using Exa.UI.Tooltips;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BlockCostsView : TooltipComponentView<BlockCosts>
 {
-    [SerializeField] private TextMeshProUGUI credits;
-    [SerializeField] private TextMeshProUGUI metals;
-    
+    public TextMeshProUGUI credits;
+    public TextMeshProUGUI metals;
+
+    public Image creditsBackground;
+    public Image metalsBackground;
+
     protected override void Refresh(BlockCosts value) {
         credits.text = value.creditCost.ToString();
         metals.text = value.metalsCost.ToString();
