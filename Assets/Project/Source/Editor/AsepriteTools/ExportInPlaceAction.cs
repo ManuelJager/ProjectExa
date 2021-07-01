209a819ec243c1924f7554089ea3f21d76f3de98
Manuel Jager <47319792+ManuelJager@users.noreply.github.com>
2021-07-02 01:07:06 +0200

    List layers

---
+++ Assets/Project/Source/Editor/AsepriteTools/ExportInPlaceAction.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using Exa.Utils;
+using UnityEditor;
+
+namespace Exa.CustomEditors {
+    [Serializable]
+    public class ExportInPlaceAction : IEditorAction {
+        private string output;
+
+        public void Render() {
+            EditorGUILayout.HelpBox($"Execution output: {output}", MessageType.Info);
+        }
+
+        public void Execute(string asepritePath, string filePath) {
+            var process = GetProcess(asepritePath, $"-b -list-layers \"{filePath}\"");
+            process.Start();
+            output = "";
+
+            var layers = GetLayers(process.StandardOutput);
+
+            output = layers.Join(" | ");
+
+            // TODO: Do the rest
+        }
+
+        private IEnumerable<string> GetLayers(StreamReader reader) {
+            while (!reader.EndOfStream) {
+                yield return reader.ReadLine();
+            }
+        }
+
+        private Process GetProcess(string asepritePath, string parameters) {
+            return new Process {
+                StartInfo = new ProcessStartInfo {
+                    FileName = asepritePath,
+                    Arguments = parameters,
+                    UseShellExecute = false,
+                    RedirectStandardOutput = true
+                }
+            };
+        }
+    }
+}
